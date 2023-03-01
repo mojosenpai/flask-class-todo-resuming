44a6dc2cdc8269a7245b95857fe8099a5b6da5e0
mojosenpai <mojosamaxiii@gmail.com>
2023-03-01 18:26:35 +0330

    initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+# main.py
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+with app.app_context():
+    db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Task(text=task_content)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'We had a problem adding your task'
+
+    elif request.method == 'GET':
+        tasks = Task.query.order_by(Task.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task = Task.query.get_or_404(id)
+    try:
+        db.session.delete(task)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting the task.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
